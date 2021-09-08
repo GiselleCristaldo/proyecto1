9878bcad87c985d285c8b0699e6a92c01bb0a65a
Giselle <gisecristaldo.3roc@gmail.com>
2021-09-08 00:49:43 -0300

    ADD se agregaron las vistas al desafio mensual por numeros

--- proyecto1/desafios/urls.py
+++ proyecto1/desafios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('enero', views.index)
+     path('<int:mes>', views.desafio_mensual_por_numero),
+    path('<str:mes>', views.desafio_mensual)
 ]
--- proyecto1/desafios/views.py
+++ proyecto1/desafios/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.template.loader import render_to_string 
 # Create your views here.
-def index(request):
-    return HttpResponse('Hola mundo!')
+desafios = {
+    'enero': 'Practicar Python',
+    'febrero': 'Leer un libro',
+    'marzo': 'Tomar mas agua',
+    'abril':'Comer mas verduras',
+    'mayo': 'Comer cuatro piezas de fruta al dia',
+    'junio': 'Hacer ejercicio media hora',
+    'julio': 'Consumir menos azucar',
+    'agosto': 'Evitar comidas fritas',
+    'septiembre': 'Dormir 8 horas diarias',
+    'octubre': 'Evitar bebidas con gas',
+    'noviembre': 'Reducir el uso de plasticos',
+    'diciembre': 'No usar bolsas',
+}
+
+
+def desafio_mensual(request, mes):
+    texto_desafio = ''
+    try: 
+        texto_desafio = desafios[mes]
+    except:
+        return HttpResponseNotFound('Mes no implementado')
+    return HttpResponse (texto_desafio)
+
+def desafio_mensual_por_numero(request, mes):
+    lista_meses = list(desafios.keys())
+    if mes > 12: 
+        return HttpResponseNotFound('Mes no encontrado. Pruebe utilizando un numero del 1 al 12')
+    else:
+         return HttpResponseRedirect(lista_meses[mes-1])
+    return HttpResponseRedirect (lista_meses[mes-1])
