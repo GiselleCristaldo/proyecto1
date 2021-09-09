615ec20878143a62f7863af2919dc641ce67d40b
Giselle <gisecristaldo.3roc@gmail.com>
2021-09-08 23:56:48 -0300

    ADD templates html, utilizamos bloques for, url, if

--- proyecto1/desafios/views.py
+++ proyecto1/desafios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.template.loader import render_to_string 
 from django.urls import reverse
+from django.template.loader import render_to_string
 # Create your views here.
 desafios = {
     'enero': 'Practicar Python',
@@ -15,33 +16,32 @@ desafios = {
     'septiembre': 'Dormir 8 horas diarias',
     'octubre': 'Evitar bebidas con gas',
     'noviembre': 'Reducir el uso de plasticos',
-    'diciembre': 'No usar bolsas',
+    'diciembre': None,
 }
 
 def index(request):
     lista_meses = list(desafios.keys())
     meses= ''
-    for mes in lista_meses:
-       meses += f'<li><a href="{mes}">{mes}</a></li>'
-    return HttpResponse("""<h1>Aplicacion Desafios</h1>"""
-    f'<ul>{meses}</ul>')
-
-    
+    return render(request, "desafios/index.html", {
+        'meses': lista_meses
+    })
+   
 
 def desafio_mensual(request, mes):
     texto_desafio = ''
+    meses= mes
     try: 
-        texto_desafio = desafios[mes]
+        return render(request,"desafios/desafio.html",
+            {'texto_desafio': desafios[mes], 
+            'mes':meses
+        } )
+           
     except:
         return HttpResponseNotFound(f'<strong>(Mes no implementado)</strong>')
     return HttpResponse (f'<h1>{texto_desafio}</h1>')
 
 def desafio_mensual_por_numero(request, mes):
     lista_meses = list(desafios.keys())
-
-    # reverse_path = reverse('desafio-mensual', args=[])
-    # print('reverse_path', reverse_path)
-
     if mes > 12: 
         return HttpResponseNotFound(f'<strong>(Mes no encontrado. Pruebe utilizando un numero del 1 al 12)</strong>')
     else:
