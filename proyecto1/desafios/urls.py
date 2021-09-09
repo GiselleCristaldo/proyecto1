4d775b0595551b163e3d94f9e6c1f7daf4618fc0
Giselle <gisecristaldo.3roc@gmail.com>
2021-09-08 22:40:38 -0300

    ADD index view, listas con sus respectivas etiquetas

--- proyecto1/desafios/urls.py
+++ proyecto1/desafios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('', views.index), #/desafio/
      path('<int:mes>', views.desafio_mensual_por_numero),
-    path('<str:mes>', views.desafio_mensual)
+    path('<str:mes>', views.desafio_mensual, name= 'desafio-mensual'),
 ]
--- proyecto1/desafios/views.py
+++ proyecto1/desafios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.template.loader import render_to_string 
+from django.urls import reverse
 # Create your views here.
 desafios = {
     'enero': 'Practicar Python',
@@ -17,19 +18,32 @@ desafios = {
     'diciembre': 'No usar bolsas',
 }
 
+def index(request):
+    lista_meses = list(desafios.keys())
+    meses= ''
+    for mes in lista_meses:
+       meses += f'<li><a href="{mes}">{mes}</a></li>'
+    return HttpResponse("""<h1>Aplicacion Desafios</h1>"""
+    f'<ul>{meses}</ul>')
+
+    
 
 def desafio_mensual(request, mes):
     texto_desafio = ''
     try: 
         texto_desafio = desafios[mes]
     except:
-        return HttpResponseNotFound('Mes no implementado')
-    return HttpResponse (texto_desafio)
+        return HttpResponseNotFound(f'<strong>(Mes no implementado)</strong>')
+    return HttpResponse (f'<h1>{texto_desafio}</h1>')
 
 def desafio_mensual_por_numero(request, mes):
     lista_meses = list(desafios.keys())
+
+    # reverse_path = reverse('desafio-mensual', args=[])
+    # print('reverse_path', reverse_path)
+
     if mes > 12: 
-        return HttpResponseNotFound('Mes no encontrado. Pruebe utilizando un numero del 1 al 12')
+        return HttpResponseNotFound(f'<strong>(Mes no encontrado. Pruebe utilizando un numero del 1 al 12)</strong>')
     else:
          return HttpResponseRedirect(lista_meses[mes-1])
     return HttpResponseRedirect (lista_meses[mes-1])
